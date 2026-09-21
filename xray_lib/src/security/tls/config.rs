6501af6251b2231270c746e97bf7faec1d2167c7
pmdev92 <pm.developer.92@gmail.com>
2026-09-22 00:14:07 +0330

    Remove unused code

--- xray_lib/src/security/tls/config.rs
+++ xray_lib/src/security/tls/config.rs
@@ -10,18 +10,3 @@ pub struct TlsConfig {
     pub pinned_peer_cert_sha256: Option<Vec<String>>,
     pub verify_peer_cert_by_name: Option<Vec<String>>,
 }
-
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
-#[serde(rename_all = "lowercase")]
-pub enum VerifyMode {
-    False,
-    True,
-    PinnedPeerCertSha256,
-    VerifyPeerCertByName,
-}
-
-impl Default for VerifyMode {
-    fn default() -> Self {
-        VerifyMode::False
-    }
-}
